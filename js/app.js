1c6e8c7b9315f5b8ae90a594f5505dddc42ca450
Brian Krabec <briankrabec@Brians-MacBook-Pro.local>
2020-03-18 20:18:10 -0500

    Setup UI and begin creating game object

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const game = {
+	
+	mainQuestionArr: [
+		"So, quick question.",
+		"What did you want to do for dinner?",
+		"That sounds good, but what about something italian tonight?",
+		"Italian it is!",
+		"Isn't it lunch time right now? Are you really working? Who's with you right now?",
+		"Okay, sorry. Sometimes I trip for no reason."
+	],
+
+	
+
+}
+
+
+
+
+// -----------------------------------------------------------------EVENTS-------------------------------------------------------------------------
